901d4987ee4cb7a9b0ada1bd47f847489c265c6f
eliakin <eliakin.am@gmail.com>
2026-05-23 13:35:39 -0300

    Adiciona menu interativo (inserir nova tabuada) com laço de repetição "Do-While".

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -6,15 +6,27 @@ public class Tabuada {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int op;
         System.out.println("Tabuada Generator");
 
-        System.out.println("Por favor, entre com a tabuada desejada:");
-        int tabuada = sc.nextInt();
-        int resultado;
-        for(int i = 0; i <=10; i++){
-            resultado = tabuada * i;
-            System.out.printf("%d X %d = %d%n", i, tabuada, resultado);
-        }
+        do {
+            System.out.println("Qual a tabuada desejada?: ");
+            int tabuada = sc.nextInt();
+            int resultado;
+            for (int i = 0; i <= 10; i++) {
+                resultado = tabuada * i;
+                System.out.printf("%d X %d = %d%n", i, tabuada, resultado);
+            }
+            do {
+                System.out.println("Inserir nova tabuada? (1 - sim | 2 - não");
+                op = sc.nextInt();
+                if(op < 1 || op > 2) {
+                    System.out.println("Opção Invalida.");
+                }
+            } while (op < 1 || op > 2);
+
+        } while (op != 2 );
+        System.out.println("Fim do Programa - Obrigado por utilizar o Tabuada Generator!");
         sc.close();
     }
 }
