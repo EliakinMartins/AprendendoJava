36c1090f8eae301cab920bdbda1ee912cd1a4d81
eliakin <eliakin.am@gmail.com>
2026-05-26 22:41:10 -0300

    Adiciona menu interativo e funcional com opções (ver saldo; depositar; sacar e sair)

--- src/Cedulas.java
+++ src/Cedulas.java
@@ -6,33 +6,61 @@ public class Cedulas {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        //As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas necessárias.
+        //Notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas necessárias.
 
-        System.out.println("Digite o valor em R$ que deseja sacar: ");
+        int op;
+        int saldo = 0;
+        int valor;
+        int deposito = 0;
 
-        int valor = sc.nextInt();
-        int n100, n50, n20, n10, n5, n2;
+        do {
+            System.out.println("1 - Ver Saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("4 - Sair");
 
-        n100 = valor / 100;
-        valor = valor % 100;
-        n50 = valor / 50;
-        valor = valor % 50;
-        n20 = valor / 20;
-        valor = valor % 20;
-        n10 = valor / 10;
-        valor = valor % 10;
-        n5 = valor / 5;
-        valor = valor % 5;
-        n2 = valor / 2;
-        valor = valor % 2;
+            op = sc.nextInt();
+            if (op == 1) {
+                System.out.printf("Saldo: R$ %d%n", saldo);
+            } else if (op == 2) {
+                System.out.println("Quanto deseja depositar?");
+                deposito = sc.nextInt();
+                saldo = saldo + deposito;
+            } else if (op == 3) {
+                System.out.printf("Seu saldo R$: %d%n", saldo);
+                System.out.println("Saque: Quanto deseja sacar ?");
 
-        System.out.printf("%d nota(s) de R$ 100,00%n",n100 );
-        System.out.printf("%d nota(s) de R$ 50,00%n",n50 );
-        System.out.printf("%d nota(s) de R$ 20,00%n",n20 );
-        System.out.printf("%d nota(s) de R$ 10,00%n",n10 );
-        System.out.printf("%d nota(s) de R$ 5,00%n",n5 );
-        System.out.printf("%d nota(s) de R$ 2,00%n",n2 );
-        System.out.printf("%d nota(s) de R$ 1,00%n",valor );
+                valor = sc.nextInt();
+                saldo = saldo - valor;
+                int n100, n50, n20, n10, n5, n2;
+
+                n100 = (int) valor / 100;
+                valor = valor % 100;
+                n50 = (int) valor / 50;
+                valor = valor % 50;
+                n20 = (int) valor / 20;
+                valor = valor % 20;
+                n10 = (int) valor / 10;
+                valor = valor % 10;
+                n5 = (int) valor / 5;
+                valor = valor % 5;
+                n2 = (int) valor / 2;
+                valor = valor % 2;
+
+                System.out.printf("%d nota(s) de R$ 100,00%n", n100);
+                System.out.printf("%d nota(s) de R$ 50,00%n", n50);
+                System.out.printf("%d nota(s) de R$ 20,00%n", n20);
+                System.out.printf("%d nota(s) de R$ 10,00%n", n10);
+                System.out.printf("%d nota(s) de R$ 5,00%n", n5);
+                System.out.printf("%d nota(s) de R$ 2,00%n", n2);
+                System.out.printf("%d nota(s) de R$ 1,00%n", (int) valor);
+
+                System.out.printf("Saldo atual: R$ %d%n",saldo);
+
+            } else {
+                System.out.println("Obrigado por utilizar!");
+            }
+        }while (op !=4);
 
         sc.close();
     }
